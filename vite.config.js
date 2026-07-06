6e2c5d6ee63d411f5c21395ad8876e41954b6792
lhc15 <lhc15@gcloud.ua.es>
2026-07-06 14:23:32 +0200

    creación pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nutrición Núñez',
+        short_name: 'NutriNúñez',
+        description: 'Tu plan de alimentación personalizado',
+        theme_color: '#2d5a3d',
+        background_color: '#f9f6f1',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
